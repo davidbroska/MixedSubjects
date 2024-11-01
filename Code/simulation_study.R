c6d4466d6af5c1f95cdd350fd8f42021ec9afe7e
David B <david.broska@gmx.de>
2024-11-01 16:06:17 -0700

    try simulation varying RSE in Yhat = Y + error

--- Code/simulation_study.R
+++ Code/simulation_study.R
@@ -197,6 +197,96 @@ rmse_plot_r = dfr_long %>%
   scale_y_continuous(breaks = seq(0,1,0.02), limits = c(0, ymax)) 
 
 
+############################################
+# Figures with PPI correlation on the x-axis
+############################################
+
+# Import averaged simulation results
+dfe = read_csv("Data/simulation_study_rse.csv.gz")
+
+# Transform data to long format and update method labels
+dfe_long = dfe %>%
+  pivot_longer(
+    cols = c(bias_ppi, bias_sil, coverage_ppi, coverage_sil, se_ppi, se_sil, rmse_ppi, rmse_sil),
+    names_to = c("metric", "method"),
+    names_sep = "_",
+    values_to = "value"
+  ) %>%
+  mutate(
+    method = case_when(
+      method == "ppi" ~ "Prediction-Powered Inference",
+      method == "sil" ~ "Silicon Sampling",
+      TRUE ~ method
+    )
+  )
+
+
+# Create Bias Plot
+bias_plot_e = dfe_long %>%
+  filter(metric == "bias") %>%
+  ggplot(aes(x = rse, y = value, color = method, linetype = method)) +
+  geom_line(linewidth = linewidth) +
+  geom_point(size = 1) +
+  labs(
+    title = " ",
+    x = "Residual standard error Yhat=Y+e",
+    y = "Bias"
+  ) +
+  scale_color_manual(values = custom_colors) +
+  scale_linetype_manual(values = custom_linetypes, guide = "none") +
+  scale_y_continuous(breaks = seq(0,1,0.02), limits = c(NA, ymax))
+
+
+# Create Coverage Plot
+coverage_plot_e = dfe_long %>%
+  filter(metric == "coverage") %>%
+  ggplot(aes(x = rse, y = value, color = method, linetype = method)) +
+  geom_hline(yintercept = 0.95, color = "darkgrey", linetype = "dashed") +
+  geom_line(linewidth = linewidth) +
+  geom_point(size = 1) +
+  labs(
+    title = " ",
+    x = "Residual standard error Yhat=Y+e",
+    y = "Coverage"
+  ) +
+  scale_color_manual(values = custom_colors) +
+  scale_linetype_manual(values = custom_linetypes, guide = "none") +
+  scale_y_continuous(labels = percent_format(accuracy = 1), breaks = seq(0,1,0.2), limits = c(0,1)) 
+
+
+# Create Standard Error Plot
+se_plot_e = dfe_long %>%
+  filter(metric == "se") %>%
+  ggplot(aes(x = rse, y = value, color = method, linetype = method)) +
+  geom_line(linewidth = linewidth) +
+  geom_point(size = 1) +
+  labs(
+    title = " ",
+    x = "Residual standard error Yhat=Y+e",
+    y = "Standard Error"
+  ) +
+  scale_color_manual(values = custom_colors) +
+  scale_linetype_manual(values = custom_linetypes, guide = "none") +
+  scale_y_continuous(breaks = seq(0,1,0.02), limits = c(0, ymax)) 
+
+
+# Create Root Mean Squared Error Plot
+rmse_plot_e = dfe_long %>%
+  filter(metric == "rmse") %>%
+  ggplot(aes(x = rse, y = value, color = method, linetype = method)) +
+  geom_line(linewidth = linewidth) +
+  geom_point(size = 1) +
+  labs(
+    title = " ",
+    x = "Residual standard error Yhat=Y+e",
+    y = "RMSE"
+  ) +
+  scale_color_manual(values = custom_colors) +
+  scale_linetype_manual(values = custom_linetypes, guide = "none") +
+  scale_y_continuous(breaks = seq(0,1,0.02), limits = c(0, ymax)) 
+
+
+
 
 ######################
 # Create combined plot 
@@ -204,7 +294,8 @@ rmse_plot_r = dfr_long %>%
 
 # Combine the plots side by side with a shared legend at the bottom
 combined_plot = bias_plot_n + se_plot_n + coverage_plot_n + rmse_plot_n +
-                bias_plot_r + se_plot_r + coverage_plot_r + rmse_plot_r +
+                #bias_plot_r + se_plot_r + coverage_plot_r + rmse_plot_r +
+                bias_plot_e + se_plot_e + coverage_plot_e + rmse_plot_e +
   plot_layout(ncol = 4, guides = "collect") &
   theme(
     legend.position = "bottom",
